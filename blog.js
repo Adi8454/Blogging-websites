15886793cc477e7269d0795886fc9de821340244
Adi8454 <144621664+Adi8454@users.noreply.github.com>
2024-04-09 11:10:19 +0530

    Add files via upload

--- blog.js
+++ blog.js
@@ -1,27 +1,61 @@
-const { Schema, model } = require("mongoose");
+const { Router } = require("express");
+const multer = require("multer");
+const path = require("path");
 
-const blogSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    body: {
-      type: String,
-      required: true,
-    },
-    coverImageURL: {
-      type: String,
-      required: false,
-    },
-    createdBy: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
+const Blog = require("../models/blog");
+const Comment = require("../models/comment");
+
+const router = Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`));
   },
-  { timestamps: true }
-);
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
 
-const Blog = model("blog", blogSchema);
+const upload = multer({ storage: storage });
 
-module.exports = Blog;
+router.get("/add-new", (req, res) => {
+  return res.render("addBlog", {
+    user: req.user,
+  });
+});
+
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+
+  return res.render("blog", {
+    user: req.user,
+    blog,
+    comments,
+  });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+});
+
+router.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, body } = req.body;
+  const blog = await Blog.create({
+    body,
+    title,
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`,
+  });
+  return res.redirect(`/blog/${blog._id}`);
+});
+
+module.exports = router;
--- user.js
+++ user.js
@@ -1,75 +1,41 @@
-const { createHmac, randomBytes } = require("crypto");
-const { Schema, model } = require("mongoose");
-const { createTokenForUser } = require("../services/authentication");
+const { Router } = require("express");
+const User = require("../models/user");
 
-const userSchema = new Schema(
-  {
-    fullName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    salt: {
-      type: String,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    profileImageURL: {
-      type: String,
-      default: "/images/default.png",
-    },
-    role: {
-      type: String,
-      enum: ["USER", "ADMIN"],
-      default: "USER",
-    },
-  },
-  { timestamps: true }
-);
+const router = Router();
 
-userSchema.pre("save", function (next) {
-  const user = this;
-
-  if (!user.isModified("password")) return;
-
-  const salt = randomBytes(16).toString();
-  const hashedPassword = createHmac("sha256", salt)
-    .update(user.password)
-    .digest("hex");
-
-  this.salt = salt;
-  this.password = hashedPassword;
-
-  next();
+router.get("/signin", (req, res) => {
+  return res.render("signin");
 });
 
-userSchema.static(
-  "matchPasswordAndGenerateToken",
-  async function (email, password) {
-    const user = await this.findOne({ email });
-    if (!user) throw new Error("User not found!");
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
 
-    const salt = user.salt;
-    const hashedPassword = user.password;
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const token = await User.matchPasswordAndGenerateToken(email, password);
 
-    const userProvidedHash = createHmac("sha256", salt)
-      .update(password)
-      .digest("hex");
-
-    if (hashedPassword !== userProvidedHash)
-      throw new Error("Incorrect Password");
-
-    const token = createTokenForUser(user);
-    return token;
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    return res.render("signin", {
+      error: "Incorrect Email or Password",
+    });
   }
-);
+});
 
-const User = model("user", userSchema);
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").redirect("/");
+});
 
-module.exports = User;
+router.post("/signup", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  await User.create({
+    fullName,
+    email,
+    password,
+  });
+  return res.redirect("/");
+});
+
+module.exports = router;
